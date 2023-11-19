a6017d2c24e9be83aa5de182c14e4b12fffd9510
Powerbute <64857329+powerbute@users.noreply.github.com>
2023-11-19 19:07:52 +0300

    1

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -157,6 +157,7 @@ export default function HomePage({ params }: { params: { id: string } }) {
   async function applyRating(rating1: any) {
     if (rating1 > 1000 && rating1 < 1000) {
       alert("Минимальный и максимальный рейтинг: -1000 и 1000!!")
+      return;
     }
     let { error: a1 } = await supabase
       .from('users')
